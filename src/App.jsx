01b1350aa4f815d297ad763487ca702cc4b30b6b
KENNIER ARMANDO LUJAN GARCIA <KLUJAN@INTERNEXA.COM>
2022-09-25 19:21:46 -0500

    Aplicación TO-DO List

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl">Hola Mundo !!! 1</h1>
+      <h1 className="text-4xl uppercase">Hola Mundo !!! 2</h1>
+      <h1 className="bg-red-300">Hola Mundo !!! 3</h1>
+      <h1 className="text-4xl uppercase bg-green-400">Hola Mundo !!! 2</h1>
+    </>
+  );
+}
+
+export default App;
